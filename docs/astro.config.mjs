3eb7757f0f93e0d2b12d81f52c1423ab2e1c4bd3
Alper Halil <aktasalper99@gmail.com>
2026-04-02 23:24:22 +0200

    add tailwind to docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,5 +1,11 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+	vite: {
+		// @ts-ignore | can't really be bothered as to why it's giving type errors. It's the literal official way to set it up...
+		plugins: [tailwindcss()]
+	}
+});
