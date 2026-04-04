8ab6e71d76865659f3d0c4f439f6c92e62055afe
Alper Halil <aktasalper99@gmail.com>
2026-04-04 12:23:23 +0200

    add favicon, meta seo bs, sitemap

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,11 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-	vite: {
-		// @ts-ignore | can't really be bothered as to why it's giving type errors. It's the literal official way to set it up...
-		plugins: [tailwindcss()]
-	}
+  site: "https://sarmal.art",
+  integrations: [sitemap()],
+  vite: {
+    // @ts-ignore | can't really be bothered as to why it's giving type errors. It's the literal official way to set it up...
+    plugins: [tailwindcss()],
+  },
 });
