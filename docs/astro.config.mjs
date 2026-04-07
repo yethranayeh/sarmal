0a1a74a51e615eea0a12e0d52363f4e738180938
Alper Halil <aktasalper99@gmail.com>
2026-04-07 19:30:51 +0200

    update UI

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
+import sarmalLight from "./src/themes/sarmal-editorial.json";
+import sarmalDark from "./src/themes/sarmal-editorial-dark.json";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://sarmal.art",
@@ -10,5 +13,16 @@ export default defineConfig({
   vite: {
     // @ts-ignore | can't really be bothered as to why it's giving type errors. It's the literal official way to set it up...
     plugins: [tailwindcss()],
+  },
+  markdown: {
+    shikiConfig: {
+      themes: {
+        // @ts-ignore | Shiki too hard to please
+        light: sarmalLight,
+        // @ts-ignore | Shiki too hard to please
+        dark: sarmalDark,
+      },
+      wrap: true,
+    },
   },
 });
